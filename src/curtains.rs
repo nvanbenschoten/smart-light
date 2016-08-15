da60c1213ff3dc39e5e77e7534e5f578b73ca4a9
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2016-08-15 02:01:32 -0400

    Mock out hardware interface for now

--- src/curtains.rs
+++ src/curtains.rs
@@ -28,6 +28,27 @@ impl Manager {
     pub fn toggle(&self) -> bool {
         let mut inner = self.inner.lock().unwrap();
         inner.open = !inner.open;
+        mock_hw::move_blinds(inner.open);
         inner.open
+    }
+}
+
+/// Mock out hardware interface until this can be implemented.
+/// All mock_hw methods block on hardware.
+mod mock_hw {
+    pub fn move_blinds(open: bool) {
+        if open {
+            open_blinds();
+        } else {
+            close_blinds();
+        }
+    }
+
+    pub fn open_blinds() {
+        println!("Open Curtains");
+    }
+
+    pub fn close_blinds() {
+        println!("Close Curtains");
     }
 }
