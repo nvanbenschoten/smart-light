c0f174b0752c3fcfeb57a6ed214a9ceab9277479
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2016-08-15 01:56:20 -0400

    Resolve mutations within HTTP handler using Arc<Mutex<T>>

--- src/http.rs
+++ src/http.rs
@@ -1,5 +1,5 @@
 use iron::prelude::*;
-use iron::{status, Listening};
+use iron::{status, Handler, Listening};
 use iron::error::HttpResult;
 use router::Router;
 use rustc_serialize::json;
@@ -8,7 +8,8 @@ use curtains::Manager;
 pub fn start(curtain_mgr: &Manager) -> HttpResult<Listening> {
     let mut router = Router::new();
 
-    setup_toggle(&mut router, curtain_mgr);
+    router.get("/status", with_manager(curtain_mgr, status));
+    router.post("/toggle", with_manager(curtain_mgr, toggle));
 
     Iron::new(router).http("0.0.0.0:8000")
 }
@@ -18,12 +19,27 @@ struct ToggleStatus {
     open: bool
 }
 
-fn setup_toggle(router: &mut Router, curtain_mgr: &Manager) {
+fn status(_: &mut Request, curtain_mgr: &Manager) -> IronResult<Response> {
+    let is_open = curtain_mgr.is_open();
+    let status = ToggleStatus{ open: is_open };
+    let payload = json::encode(&status).unwrap();
+    Ok(Response::with((status::Ok, payload)))
+}
+
+fn toggle(_: &mut Request, curtain_mgr: &Manager) -> IronResult<Response> {
+    let toggle = curtain_mgr.toggle();
+    let status = ToggleStatus{ open: toggle };
+    let payload = json::encode(&status).unwrap();
+    Ok(Response::with((status::Ok, payload)))
+}
+
+fn with_manager<F1>(curtain_mgr: &Manager, f: F1) -> Box<Handler>
+    where F1: Fn(&mut Request, &Manager) -> IronResult<Response> + Send + Sync + 'static {
+
+    // Moving an immutable clone of the curtain::Manager into the closure
+    // is required to create an Fn instead of an FnMut.
     let curtain_mgr_clone = curtain_mgr.clone();
-    router.get("/toggle", move |req: &mut Request| -> IronResult<Response> {
-        let toggle = curtain_mgr_clone.toggle();
-        let status = ToggleStatus{ open: toggle };
-        let payload = json::encode(&status).unwrap();
-        Ok(Response::with((status::Ok, payload)))
+    return Box::new(move |req: &mut Request| -> IronResult<Response> {
+        f(req, &curtain_mgr_clone)
     });
 }
