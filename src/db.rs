6c96143c4332b9aebea83238b40d8e58984f4fa5
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2016-08-15 04:55:58 -0400

    Add id field to Action struct

--- src/db.rs
+++ src/db.rs
@@ -13,7 +13,8 @@ pub struct Service {
 #[allow(dead_code)]
 #[derive(Debug)]
 pub struct Action {
-    day: Weekday,
+    id:   i32,
+    day:  Weekday,
     time: DateTime<Local>,
     open: bool,
 }
@@ -29,10 +30,11 @@ impl Service {
         let conn = try!(Connection::connect("postgresql://root@localhost:26257/smart_light", SslMode::None)
             .map_err(|e| ServiceError::Connect(e)));
         try!(conn.execute("CREATE TABLE IF NOT EXISTS actions (
-                day INT,
-                time TIMESTAMP,
-                open BOOL,
-                PRIMARY KEY (day, time)
+                id   SERIAL PRIMARY KEY,
+                day  INT NOT NULL,
+                time TIMESTAMP NOT NULL,
+                open BOOL NOT NULL,
+                UNIQUE INDEX (day, time)
             )", &[])
             .map_err(|e| ServiceError::Exec(e)));
         Ok(Service {
@@ -44,20 +46,21 @@ impl Service {
     #[allow(dead_code)]
     pub fn write_action(&self, action: &Action) -> Result<(), Error> {
         self.connection
-            .execute("INSERT INTO actions (day, time, open) VALUES ($1, $2, $3)",
-                     &[&action.day.num_days_from_monday(), &action.time, &action.open])
+            .execute("INSERT INTO actions (id, day, time, open) VALUES ($1, $2, $3)",
+                     &[&action.id, &action.day.num_days_from_monday(), &action.time, &action.open])
             .map(|_| ())
     }
 
     #[allow(dead_code)]
     pub fn get_actions(&self) -> Result<Vec<Action>, Error> {
-        let rows = try!(self.connection.query("SELECT day, time, open FROM actions", &[]));
+        let rows = try!(self.connection.query("SELECT id, day, time, open FROM actions", &[]));
         let mut actions = Vec::new();
         for row in &rows {
             actions.push(Action {
-                day:  Weekday::from_i64(row.get(0)).unwrap(),
-                time: row.get(1),
-                open: row.get(2),
+                id:   row.get(0),
+                day:  Weekday::from_i64(row.get(1)).unwrap(),
+                time: row.get(2),
+                open: row.get(3),
             });
         }
         Ok(actions)
